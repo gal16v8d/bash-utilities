a3ef39fff403ec0e0d6bba032670319b1e8b45af
alexg <alex.galvis.sistemas@gmail.com>
2022-10-20 18:20:08 -0500

    Migrate dw1-svc to check heroku alternatives

--- setup-repo-github.sh
+++ setup-repo-github.sh
@@ -1,3 +1,4 @@
+git clone git@github.com:gal16v8d/dw1-svc.git
 git clone git@github.com:gal16v8d/flag-svc.git
 git clone git@github.com:gal16v8d/testmvc.git
 git clone git@github.com:JessicaJSI/hyperblog.git
