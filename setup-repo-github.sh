2b701a92be6a5203c7627a611d5d300921111bd6
alexg <alex.galvis.sistemas@gmail.com>
2022-10-25 20:17:28 -0500

    Adding github profile repo

--- setup-repo-github.sh
+++ setup-repo-github.sh
@@ -1,4 +1,5 @@
 git clone git@github.com:gal16v8d/dw1-svc.git
 git clone git@github.com:gal16v8d/flag-svc.git
+git clone git@github.com:gal16v8d/gal16v8d.git
 git clone git@github.com:gal16v8d/testmvc.git
 git clone git@github.com:JessicaJSI/hyperblog.git
