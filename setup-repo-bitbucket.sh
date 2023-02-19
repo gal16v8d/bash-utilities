50c79b375fac21f9cc0b13ba35a92b78905d451b
alexg <alex.galvis.sistemas@gmail.com>
2023-02-19 11:11:38 -0500

    Selenium-bot is removed, all the logic was migrated to serenity-bdd repo

--- setup-repo-bitbucket.sh
+++ setup-repo-bitbucket.sh
@@ -49,7 +49,6 @@ git clone git@bitbucket.org:gal16v8d/property-util.git
 git clone git@bitbucket.org:gal16v8d/qr-app.git
 git clone git@bitbucket.org:gal16v8d/rabbitmq.git
 git clone git@bitbucket.org:gal16v8d/redis.git
-git clone git@bitbucket.org:gal16v8d/selenium-bot.git
 git clone git@bitbucket.org:gal16v8d/serenity-bdd.git
 git clone git@bitbucket.org:gal16v8d/sort.git
 git clone git@bitbucket.org:gal16v8d/test-process.git
